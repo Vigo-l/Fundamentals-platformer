2d2890faea7a6060f72fe689c6cc7994822a6df7
AlexJeBoyy <a.strikwerda@sintlucasedu.nl>
2024-09-25 10:47:37 +0200

    Win scene

--- Platformer fund/Assets/Alex Scripts/SceneLoader.cs
+++ Platformer fund/Assets/Alex Scripts/SceneLoader.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public static SceneLoader instance;
     public void LvL1()
     {
-        SceneManager.LoadScene("");
+        SceneManager.LoadScene("Level1");
     }
-    
+    public void Win()
+    {
+        SceneManager.LoadScene("Win");
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
 }
